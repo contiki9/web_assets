8055880f3c85676c28ca4a1318d776f19255888c
hirano <contiki9@gmail.com>
2016-04-02 11:23:22 +0900

    バージョンアップ -mixinアニメーションの追加 -moduleの読み込み追加 -ogpに画像追加

--- html/assets/js/module.js
+++ html/assets/js/module.js
@@ -3,7 +3,42 @@ $(document).ready(function(){
     $('a').each(function() {
       var a = this;
       if (a.js-origin !== location.origin) {
-        $(a).attr('target', '_blank');
-      }
-    });
-});
+//ブラウザサイズ別のJSファイル読み込み管理
+////////////////////////////////////////////////
+//jQuery(document).ready(function($) {
+//	//PC環境の場合
+//	if (window.matchMedia( '(min-width: 769px)' ).matches) {　//切り替える画面サイズ
+//		$.ajax({
+//			url: 'js/pc.js',
+//			dataType: 'script',
+//			cache: false
+//	   });
+//	//モバイル環境の場合
+//	} else {
+//	    $.ajax({
+//			url: 'js/sp.js',
+//			dataType: 'script',
+//			cache: false
+//		});
+//	};
+//});
+
+
+//ブラウザサイズ別のJSファイル読み込み管理
+////////////////////////////////////////////////
+//$(function () {
+//	$(window).bind("load resize", init);//ウィンドウが『読込み』もしくは『ウィンドウサイズ変更』された時、関数『init』を実行
+//	function init() {//下記の処理を関数『init』として定義する
+//		var _width = $(window).width();//デバイス（ウィンドウ）幅を取得
+//		if (_width <= 480) {
+//			//デバイス（ウィンドウ）幅が480px以下の時の処理
+//
+//		} else {
+//
+//		}
+//	}//init
+//});
+
+
+
+
